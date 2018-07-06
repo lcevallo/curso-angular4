96486a891237d16fb86ea7e23bc0408cd2e07ad4
Luis Cevallos <lcevallo@gmail.com>
2018-07-05 22:16:19 -0500

    Fase inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+import { EmpleadoComponent } from './empleado/empleado.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { FrutaComponent } from './fruta/fruta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrutaComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+        selector: 'empleado-tag',
+        templateUrl: './empleado.component.html',
+
+})
+export class EmpleadoComponent{
+        public titulo ='Titulo del componente empleado';
+}
